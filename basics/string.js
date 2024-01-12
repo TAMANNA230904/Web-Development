c776bf4ee15349b1f06cb2c1c36c0fee6deadf3c
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-01-12 10:31:24 +0000

    strings done

---
+++ basics/string.js
@@ -0,0 +1,14 @@
+const name="Tamanna"
+const repocnt=50
+console.log(name+repocnt+"Value");
+console.log(`Hello my name is ${name}`)
+const newName =new String("HelloWorld")
+console.log(newName[5])
+console.log(newName.length)
+console.log(newName.toUpperCase());
+console.log(newName.charAt(7));
+const news=newName.substring(0,4);
+console.log(news)
+//slice(-6,3),trim to remove starting and ending spaces spaces 
+//replace('x','h'),includes('hello')-true
+
