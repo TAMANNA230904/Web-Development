15788213270a7ee72aa78a2b07df8c25068bcab2
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-01-12 10:53:47 +0000

    numbers and math

---
+++ basics/number.js
@@ -0,0 +1,23 @@
+const score=400
+const new_score=new Number(100)
+// console.log(score)
+// console.log(score.toFixed(5))//400.00000
+// console.log(score.toString().length);
+
+
+const x=123.8877
+//console.log(x.toPrecision(3))//how many  digits after rounded off required
+//Maths
+// console.log(Math);
+// console.log(Math.abs(-4))//4
+// console.log(Math.ceil(4.2))//5-higher integer
+// console.log(Math.floor(4.6))//4-lower integer
+// console.log(Math.min(2,11,9,1))
+
+console.log(Math.random())//gives random values between 0 and 1
+console.log(Math.floor(Math.random()*10))
+
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
