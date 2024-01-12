563ee433c0951c362f2be6687dabdebcb3eaf018
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-01-12 11:18:06 +0000

    date and time in js

---
+++ basics/dateandtime.js
@@ -0,0 +1,16 @@
+//Date
+let date=new Date()
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
+
+let myDate=new Date(2025,8,23,7,7)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+
+let mytimestamp=Date.now()//gives preset time from 1970 1 Jan till now in milli sec
+// console.log(mytimestamp)
+// console.log(myDate.getTime())
+
+let anydate=new Date()
+console.log(anydate.getMonth())
