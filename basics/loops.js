320e46af18b5232ce9536478f74ccc2e9dda0002
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-01-15 08:14:44 +0000

    loops

---
+++ basics/loops.js
@@ -0,0 +1,31 @@
+//for
+// const arr=[1,8,32,14]
+// for (let index = 0; index < arr.length; index++) {
+//     console.log(arr[index])
+    
+// }
+//if out of bound-element undefined
+
+//break and continue
+// for (let index = 1; index <=20; index++) {
+//     if(index==5){
+//         console.log("5 is Detected");
+//         //break;//get out of loop
+//         continue;//skip to next iteration
+//     }
+//     console.log(`Value of index is ${index}`);
+    
+// }
+
+/*------While Loop---------*/
+let x=1;
+while (x<=10) {
+    console.log(`Value of index is ${x}`)
+    x+=2
+}
+
+let score=11
+do {
+    console.log(score)
+    score++
+} while (score<=10);
