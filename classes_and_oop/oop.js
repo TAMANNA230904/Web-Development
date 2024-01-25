54bbbf8f47f6a553b850b1b8a887566ab4e48544
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-01-25 16:08:22 +0000

    constructor

---
+++ classes_and_oop/oop.js
@@ -0,0 +1,28 @@
+const user={
+    username:"tammy",
+    logincnt:8,
+    signedIn:true,
+    getUserDetails:function(){
+        console.log('Got user details from database ')
+    }
+}
+console.log(user.getUserDetails());
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+console.log(userTwo);
+
