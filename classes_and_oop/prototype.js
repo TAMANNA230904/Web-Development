6258e638f89eae30b2545316be69dc615f28bd96
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-01-27 08:33:43 +0000

    inheritance

--- classes_and_oop/prototype.js
+++ classes_and_oop/prototype.js
@@ -1,9 +1,3 @@
-// let myName='Tamanna   '
-// myName.prototype.truelength=function(){
-
-// }
-// console.log(myName.truelength);
-
 let myHero=["thor","ironman"]
 
 let heroPower= {
@@ -23,7 +17,46 @@ Object.prototype.tammy= function(){
 //     console.log(`hitesh is present in all objects`);
 // }
 
+Array.prototype.heytammy=function(){
+    console.log('Tammy says hello!!');
+}
+//tammy is defined on object which its child will also get  
 myHero.tammy();
+heroPower.tammy();  
+//but on assigning fxn to a lower member object will not get that property
+myHero.heytammy()
+//heroPower.heytammy()
 
 
+/*----INHERITANCE-----*/
+const user={
+    username:'tiger shroff',
+    email:'tiger@gamil.com'
+}
+const Teacher={
+    give_lecture:true
+}
 
+const TeachingSupport={
+    isAvailable:true
+}
+
+const TASupport={
+    make_assignment:'JS Assignment',
+    fullTime:true,
+    //taking reference from another object
+    __proto__: TeachingSupport   
+}
+
+Teacher.__proto__=user
+
+//Modern Syntax
+Object.setPrototypeOf(TeachingSupport,Teacher)//former gets the properties of latter
+
+
+let anotherUsername='TamannaSheikh      '
+String.prototype.truelength=function(){
+    console.log(`Truelength is: ${this.trim().length}`)
+}
+
+anotherUsername.truelength()
